8d0c9b468c0205744955df675210ab85aabe8ef8
daniel beitel <daniel beitel>
2017-05-08 20:52:55 -0400

    updated raiders_test.py

---
+++ raiders_test.py
@@ -0,0 +1,21 @@
+import unittest
+
+import unh698
+
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = raiders.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_home_page(self): #tests homepage we are building
+        # Render the / path of the website
+        rv = self.app.get('/')
+        # Chech that the page contians the desired phrase
+        assert b'Raiders Website' in rv.data #looks for this phrase
+
+if __name__ == '__main__':
+    unittest.main() #unit test for main
