599ec8914d2ebd061bf3afc219501b1d72d118c9
daniel beitel <daniel beitel>
2017-05-13 23:02:56 -0400

    changes

--- raiders.py
+++ raiders.py
@@ -1,10 +1,20 @@
-from flask import Flask #imports Flask
+from flask import Flask, render_template #imports Flask and template
 app = Flask(__name__) #imports Flask app with name we specify
 
 @app.route('/')
 def home(): #defining the home function
-	return "Raiders Website" #when if works will return this
+	return render_template('index.html') #when if works will return this
 #this is where we will build other pages later on
+
+@app.route('/main')
+def main():#defines main
+	return render_template('main.html') #calls main template
+
+@app.route('/my-topic')
+def page(): #defines page
+	return render_template('topic.html')
+
+
 if __name__ == "__main__":
 	app.run(debug=True,host='0.0.0.0', port=8080)	
 #if the name ==main, run the app, host = 0.0.0.0 through port 8080
